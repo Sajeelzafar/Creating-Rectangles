98d1af2069b1ea470b0a069590f39de568589856
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-12-11 20:00:45 +0500

    Install and fix linter errors (best practices)

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
-const uploadButton = document.getElementById("uploadButton");
-const uploadedFile = document.getElementById("uploadedFile");
-const canvas = document.getElementById("chartContainer");
-const timeExecution = document.querySelector(".timeExecution");
+const uploadButton = document.getElementById('uploadButton');
+const uploadedFile = document.getElementById('uploadedFile');
+const canvas = document.getElementById('chartContainer');
+const timeExecution = document.querySelector('.timeExecution');
 const obj = [];
-let numberofRectangles;
 
 uploadButton.addEventListener('click', () => {
-	let t1 = performance.now();
-    Papa.parse((uploadedFile).files[0],
+  const t1 = performance.now();
+  // eslint-disable-next-line
+  Papa.parse((uploadedFile).files[0],
     {
-        download: true,
-        header: true,
-        skipEmptyLines: true,
-		
-        complete: function(results){
-			for(let i=0; i<results.data.length; i++){
-				obj.push({
-					type: "rangeArea",
-					markerSize: 0,
-					highlightEnabled: false,
-					toolTipContent: null,
-					
-					dataPoints: [
-						{ x:parseInt(results.data[i].x1), y: [parseInt(results.data[i].y1), parseInt(results.data[i].y2)] },
-						{ x:parseInt(results.data[i].x2), y: [parseInt(results.data[i].y1), parseInt(results.data[i].y2)] },
-					  ]
-				})
-			}
-			const chart = new CanvasJS.Chart(canvas, {
-				title:{
-				  text: "Displaying Rectangles"
-				},
-				axisX:{
-			   		minimum: 0,
-			   		maximum: 1000
-				},
-				axisY:{
-					minimum: 0,
-					maximum: 1000
-				},
-				data: obj,
-				
-			});
-			  
-			chart.render();
-			let t2 = performance.now();
-			let elapsed = (t2 - t1)/1000;
-			timeExecution.innerHTML = `It took ${elapsed} seconds to print ${obj.length} rectangles`;
+      download: true,
+      header: true,
+      skipEmptyLines: true,
+
+      complete(results) {
+        for (let i = 0; i < results.data.length; i += 1) {
+          obj.push({
+            type: 'rangeArea',
+            markerSize: 0,
+            highlightEnabled: false,
+            toolTipContent: null,
+
+            dataPoints: [
+              {
+                x: parseInt((results.data[i].x1), 10),
+                y: [parseInt((results.data[i].y1), 10), parseInt((results.data[i].y2), 10)],
+              },
+              {
+                x: parseInt((results.data[i].x2), 10),
+                y: [parseInt((results.data[i].y1), 10), parseInt((results.data[i].y2), 10)],
+              },
+            ],
+          });
         }
-    })
-	
-})
+        // eslint-disable-next-line
+        const chart = new CanvasJS.Chart(canvas, {
+          title: {
+            text: 'Displaying Rectangles',
+          },
+          axisX: {
+            minimum: 0,
+            maximum: 1000,
+          },
+          axisY: {
+            minimum: 0,
+            maximum: 1000,
+          },
+          data: obj,
+
+        });
+
+        chart.render();
+        const t2 = performance.now();
+        const elapsed = (t2 - t1) / 1000;
+        timeExecution.innerHTML = `It took ${elapsed} seconds to print ${obj.length} rectangles`;
+      },
+    });
+});
--- jsontocsv.js
+++ jsontocsv.js
@@ -1,37 +1,36 @@
-const quantity = document.getElementById("quantity");
-const nameCSV = document.getElementById("nameCSV");
+const quantity = document.getElementById('quantity');
+const nameCSV = document.getElementById('nameCSV');
 
-let obj = [];
+const obj = [];
 
-const downloadButton = document.getElementById("downloadButton");
+const downloadButton = document.getElementById('downloadButton');
 
-function downloadCSV(filename, csvData){
-    const rectangles = document.createElement("a");
-    rectangles.setAttribute("href", `data:text/csv;charset=utf-8,${csvData}`);
-    rectangles.setAttribute("download", filename);
-    document.body.appendChild(rectangles);
-    rectangles.click();
-    document.body.removeChild(rectangles);
+function downloadCSV(filename, csvData) {
+  const rectangles = document.createElement('a');
+  rectangles.setAttribute('href', `data:text/csv;charset=utf-8,${csvData}`);
+  rectangles.setAttribute('download', filename);
+  document.body.appendChild(rectangles);
+  rectangles.click();
+  document.body.removeChild(rectangles);
 }
 
-downloadButton.addEventListener("click", () => {
-    for(let i=0; i < parseInt(quantity.value); i+=1){
-        let x1 = Math.floor(Math.random()*1000);
-        let x2 = Math.floor(Math.random()*1000);
-        let y1 = Math.floor(Math.random()*1000);
-        let y2 = Math.floor(Math.random()*1000);
-        obj.push({
-            x1,
-            x2,
-            y1,
-            y2,
-        });
-    }
-    if(parseInt(quantity.value) > 0 && parseInt(quantity.value)<1000000){
-        downloadCSV(`${nameCSV.value}.csv`, json2csv.parse(obj));
-    }
-    else{
-        alert("Please select the value in given range");
-    }
-    
-})
+downloadButton.addEventListener('click', () => {
+  for (let i = 0; i < parseInt((quantity.value), 10); i += 1) {
+    const x1 = Math.floor(Math.random() * 1000);
+    const x2 = Math.floor(Math.random() * 1000);
+    const y1 = Math.floor(Math.random() * 1000);
+    const y2 = Math.floor(Math.random() * 1000);
+    obj.push({
+      x1,
+      x2,
+      y1,
+      y2,
+    });
+  }
+  if (parseInt((quantity.value), 10) > 0 && parseInt((quantity.value), 10) < 1000000) {
+    // eslint-disable-next-line
+    downloadCSV(`${nameCSV.value}.csv`, json2csv.parse(obj));
+  } else {
+    alert('Please select the value in given range');
+  }
+});
