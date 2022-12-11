4db324fe8e91ea093df6ded6969bc84d4e315a43
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-12-11 19:36:41 +0500

    Add styling to project

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const uploadButton = document.getElementById("uploadButton");
 const uploadedFile = document.getElementById("uploadedFile");
 const canvas = document.getElementById("chartContainer");
+const timeExecution = document.querySelector(".timeExecution");
 const obj = [];
+let numberofRectangles;
 
 uploadButton.addEventListener('click', () => {
+	let t1 = performance.now();
     Papa.parse((uploadedFile).files[0],
     {
         download: true,
         header: true,
         skipEmptyLines: true,
+		
         complete: function(results){
 			for(let i=0; i<results.data.length; i++){
 				obj.push({
@@ -16,13 +20,14 @@ uploadButton.addEventListener('click', () => {
 					markerSize: 0,
 					highlightEnabled: false,
 					toolTipContent: null,
+					
 					dataPoints: [
 						{ x:parseInt(results.data[i].x1), y: [parseInt(results.data[i].y1), parseInt(results.data[i].y2)] },
 						{ x:parseInt(results.data[i].x2), y: [parseInt(results.data[i].y1), parseInt(results.data[i].y2)] },
 					  ]
 				})
 			}
-			var chart = new CanvasJS.Chart(canvas, {
+			const chart = new CanvasJS.Chart(canvas, {
 				title:{
 				  text: "Displaying Rectangles"
 				},
@@ -35,9 +40,14 @@ uploadButton.addEventListener('click', () => {
 					maximum: 1000
 				},
 				data: obj,
+				
 			});
 			  
 			chart.render();
+			let t2 = performance.now();
+			let elapsed = (t2 - t1)/1000;
+			timeExecution.innerHTML = `It took ${elapsed} seconds to print ${obj.length} rectangles`;
         }
     })
+	
 })
--- jsontocsv.js
+++ jsontocsv.js
@@ -1,16 +1,8 @@
+const quantity = document.getElementById("quantity");
+const nameCSV = document.getElementById("nameCSV");
+
 let obj = [];
-for(let i=0; i < 1000000; i+=1){
-    let x1 = Math.floor(Math.random()*1000);
-    let x2 = Math.floor(Math.random()*1000);
-    let y1 = Math.floor(Math.random()*1000);
-    let y2 = Math.floor(Math.random()*1000);
-    obj.push({
-        x1,
-        x2,
-        y1,
-        y2,
-    });
-}
+
 const downloadButton = document.getElementById("downloadButton");
 
 function downloadCSV(filename, csvData){
@@ -23,5 +15,23 @@ function downloadCSV(filename, csvData){
 }
 
 downloadButton.addEventListener("click", () => {
-    downloadCSV("rectangular-coords-for-practice.csv", json2csv.parse(obj));
+    for(let i=0; i < parseInt(quantity.value); i+=1){
+        let x1 = Math.floor(Math.random()*1000);
+        let x2 = Math.floor(Math.random()*1000);
+        let y1 = Math.floor(Math.random()*1000);
+        let y2 = Math.floor(Math.random()*1000);
+        obj.push({
+            x1,
+            x2,
+            y1,
+            y2,
+        });
+    }
+    if(parseInt(quantity.value) > 0 && parseInt(quantity.value)<1000000){
+        downloadCSV(`${nameCSV.value}.csv`, json2csv.parse(obj));
+    }
+    else{
+        alert("Please select the value in given range");
+    }
+    
 })
